11b0fd2d5dd1a7ef54332175a76cff82806bf77b
kovila77 <354981@mail.ru>
2020-10-05 23:21:23 +0500

    Made ToneTextBox

--- LabColorPickerAndClock/ColorPicker.xaml.cs
+++ LabColorPickerAndClock/ColorPicker.xaml.cs
@@ -20,9 +20,31 @@ namespace LabColorPickerAndClock
     /// </summary>
     public partial class ColorPicker : UserControl
     {
+        public InputType InputType
+        {
+            get { return ttb0.ColorTypeInput; }
+            set
+            {
+                if (value != ttb0.ColorTypeInput)
+                {
+                    ttb0.ColorTypeInput = ttb1.ColorTypeInput = ttb2.ColorTypeInput = value;
+                }
+            }
+        }
+
         public ColorPicker()
         {
             InitializeComponent();
+        }
+
+        private void RadioButtonDec_Checked(object sender, RoutedEventArgs e)
+        {
+            ttb0.ColorTypeInput = ttb1.ColorTypeInput = ttb2.ColorTypeInput = InputType.Dec;
+        }
+
+        private void RadioButtonHex_Checked(object sender, RoutedEventArgs e)
+        {
+            ttb0.ColorTypeInput = ttb1.ColorTypeInput = ttb2.ColorTypeInput = InputType.Hex;
         }
     }
 }
--- LabColorPickerAndClock/ToneTextBox .cs
+++ LabColorPickerAndClock/ToneTextBox .cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,70 +12,108 @@ namespace LabColorPickerAndClock
 {
     public class ToneTextBox : TextBox
     {
-        public enum InputType
+        private const int MAX_COLOR_VAL = 255;
+        private const int MIN_COLOR_VAL = 0;
+        private const string AllovedSymbolsHex = "0123456789abcdef";
+        private InputType _inputType = InputType.Dec;
+
+        public InputType ColorTypeInput
         {
-            Dec,
-            Hex,
+            get { return _inputType; }
+            set
+            {
+                if (value != _inputType)
+                {
+                    if (value == InputType.Dec)
+                    {
+                        _inputType = InputType.Dec;
+                        this.Text = int.Parse(this.Text, NumberStyles.AllowHexSpecifier | NumberStyles.HexNumber).ToString();
+                    }
+                    else
+                    {
+                        _inputType = InputType.Hex;
+                        this.Text = Convert.ToString(int.Parse(this.Text), 16);
+                    }
+                }
+            }
         }
 
-        public InputType ColorTypeInput { get; set; }
+        public uint Value
+        {
+            get { return uint.Parse(this.Text); }
+            set
+            {
+                if (value > MAX_COLOR_VAL)
+                {
+                    this.Text = _inputType == InputType.Dec ? MAX_COLOR_VAL.ToString() : Convert.ToString(MAX_COLOR_VAL, 16);
+                }
+                else
+                {
+                    if (_inputType == InputType.Dec)
+                    {
+                        this.Text = value.ToString();
+                    }
+                    else
+                    {
+                        this.Text = Convert.ToString(value, 16);
+                    }
+                }
+            }
+        }
 
         public ToneTextBox()
         {
-            ColorTypeInput = InputType.Dec;
+            this.Text = "0";
         }
-        //protected override void OnPreviewKeyDown(System.Windows.Input.KeyEventArgs e)
-        //{
-        //    if (!(e.Key == Key.D0 || e.Key == Key.D5
-        //        || e.Key == Key.D1 || e.Key == Key.D6
-        //        || e.Key == Key.D2 || e.Key == Key.D7
-        //        || e.Key == Key.D3 || e.Key == Key.D8
-        //        || e.Key == Key.D4 || e.Key == Key.D9
-        //        || e.Key == Key.Back || e.Key == Key.Delete
-        //        ))
-        //        e.Handled = true;
 
-        //    base.OnPreviewKeyDown(e);
-        //}
-
-        //protected override void OnPreviewTextInput(System.Windows.Input.TextCompositionEventArgs e)
-        //{
-        //    e.Handled = "0123456789".IndexOf(e.Text) < 0;
-        //    base.OnPreviewTextInput(e);
-        //}
-        protected override void OnPreviewTextInput(System.Windows.Input.TextCompositionEventArgs e)
+        protected override void OnTextChanged(TextChangedEventArgs e)
         {
-            if ("0123456789".IndexOf(e.Text) < 0)
+            for (int i = 0; i < this.Text.Length; i++)
             {
-                e.Handled = true;
+                if (!IsCorrectDigit(this.Text[i]))
+                {
+                    this.Text = this.Text.Substring(0, i);
+                    break;
+                }
             }
-            //else
-            //{
-            //    int t = Text
-            //}
-            base.OnPreviewTextInput(e);
+
+            int x;
+            if (!NumberTryParse(this.Text, out x) || x > MAX_COLOR_VAL)
+            {
+                if (this.Text.Length > 0)
+                {
+                    this.Text = _inputType == InputType.Dec ? MAX_COLOR_VAL.ToString() : Convert.ToString(MAX_COLOR_VAL, 16);
+                }
+                else
+                {
+                    this.Text = MIN_COLOR_VAL.ToString();
+                }
+            }
+            base.OnTextChanged(e);
         }
 
-        //protected override void OnTextInput(TextCompositionEventArgs e)
-        //{
-        //    int x;
-        //    if (!int.TryParse(this.Text + e.Text, out x))
-        //    {
-        //        e.Handled = true;
-        //    }
-        //    else
-        //    {
-        //        if ()
-        //    }
-        //    base.OnTextInput(e);
-        //}
+        private bool IsCorrectDigit(char ch)
+        {
+            if (this._inputType == InputType.Dec)
+            {
+                return char.IsDigit(ch);
+            }
+            else
+            {
+                return AllovedSymbolsHex.Contains(ch);
+            }
+        }
 
-        //protected override void OnTextChanged(TextChangedEventArgs e)
-        //{
-        //    int x;
-        //    if (!int.TryParse(Text, out x))
-        //        e.
-        //    base.OnTextChanged(e);
-        //}
+        private bool NumberTryParse(string str, out int x)
+        {
+            if (this._inputType == InputType.Dec)
+            {
+                return int.TryParse(str, out x);
+            }
+            else
+            {
+                return int.TryParse(str, NumberStyles.AllowHexSpecifier | NumberStyles.HexNumber, CultureInfo.InvariantCulture, out x);
+            }
+        }
     }
 }
