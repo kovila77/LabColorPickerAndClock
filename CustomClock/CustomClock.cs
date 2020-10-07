be3d25c0f616b9d6afbf23b45d8ecd046697c0c9
kovila77 <354981@mail.ru>
2020-10-07 10:48:36 +0500

    made lines throught circles

--- CustomClock/CustomClock.cs
+++ CustomClock/CustomClock.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -53,43 +54,82 @@ namespace CustomClock
 
         protected override void OnRender(DrawingContext drawingContext)
         {
-            Point center = new Point(this.Width / 2, this.Height / 2);
-            double radius = Math.Min(this.Width, this.Height);
+            Point center = new Point(this.ActualWidth / 2, this.ActualHeight / 2);
+            double radius = Math.Min(this.ActualWidth, this.ActualHeight) / 2;
 
+            // Main circle
             drawingContext.DrawEllipse(
                 this.Background,
-                new Pen(this.BorderBrush, radius*0.02),
+                new Pen(this.BorderBrush, radius * 0.02),
                 center,
                 radius,
                 radius);
 
-            double angle = 0;
-            for (int i = 0; i < 12; i++, angle += Math.PI / 6)
-            {
-                drawingContext.DrawLine(
+            //double angle = 0;
+            //for (int i = 1; i <= 12; i++, angle += Math.PI / 6)
+            //{
+            //    drawingContext.DrawLine(
+            //        new Pen(this.BorderBrush, radius * 0.02),
+            //        new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.10 * (Math.Cos(angle))
+            //                , center.Y + radius * (Math.Sin(angle)) - radius * 0.10 * (Math.Sin(angle))),
+            //        new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.30 * (Math.Cos(angle))
+            //                , center.Y + radius * (Math.Sin(angle)) - radius * 0.30 * (Math.Sin(angle))));
+            //}
+
+            //angle = 0;
+            //for (int i = 0; i < 12; i++)
+            //{
+            //    angle += Math.PI / 30;
+            //    for (int j = 0; j < 4; j++, angle += Math.PI / 30)
+            //    {
+            //        drawingContext.DrawLine(
+            //            new Pen(this.BorderBrush, radius * 0.01),
+            //            new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.10 * (Math.Cos(angle))
+            //                    , center.Y + radius * (Math.Sin(angle)) - radius * 0.10 * (Math.Sin(angle))),
+            //            new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.20 * (Math.Cos(angle))
+            //                    , center.Y + radius * (Math.Sin(angle)) - radius * 0.20 * (Math.Sin(angle))));
+            //    }
+            //}
+
+            // Small line
+            var p = new Pen(this.BorderBrush, radius * 0.02);
+            var ds = DashStyles.Dash.Clone();
+            ds.Dashes[0] = 1;
+            ds.Dashes[1] = 3.658;
+            p.DashStyle = ds;
+            p.DashCap = 0;
+
+            drawingContext.PushTransform(new RotateTransform(0.57, center.X, center.Y));
+            drawingContext.DrawEllipse(
+                null,
+                p,
+                center,
+                radius * 0.889,
+                radius * 0.889);
+            drawingContext.Pop();
+
+            // Big line
+            var p1 = new Pen(this.BorderBrush, radius*0.2);
+            var ds1 = DashStyles.Dash.Clone();
+            ds1.Dashes[0] = 0.1;
+            ds1.Dashes[1] = 1.995;
+            p1.DashStyle = ds1;
+            p1.DashCap = 0;
+
+            drawingContext.PushTransform(new RotateTransform(-16.4, center.X, center.Y));
+            drawingContext.DrawEllipse(
+                null,
+                p1,
+                center,
+                radius * 0.80,
+                radius * 0.80);
+            drawingContext.Pop();
+
+            drawingContext.DrawEllipse(this.BorderBrush,
                     new Pen(this.BorderBrush, radius * 0.02),
-                    new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.10 * (Math.Cos(angle))
-                            , center.Y + radius * (Math.Sin(angle)) - radius * 0.10 * (Math.Sin(angle))),
-                    new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.30 * (Math.Cos(angle))
-                            , center.Y + radius * (Math.Sin(angle)) - radius * 0.30 * (Math.Sin(angle))));
-            }
-
-            angle = 0;
-            for (int i = 0; i < 12; i++)
-            {
-                angle += Math.PI / 30;
-                for (int j = 0; j < 4; j++, angle+=Math.PI/30)
-                {
-                    drawingContext.DrawLine(
-                        new Pen(this.BorderBrush, radius * 0.01),
-                        new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.10 * (Math.Cos(angle))
-                                , center.Y + radius * (Math.Sin(angle)) - radius * 0.10 * (Math.Sin(angle))),
-                        new Point(center.X + radius * (Math.Cos(angle)) - radius * 0.20 * (Math.Cos(angle))
-                                , center.Y + radius * (Math.Sin(angle)) - radius * 0.20 * (Math.Sin(angle))));
-                }
-            }
-
-
+                    center,
+                    radius * 0.02,
+                    radius * 0.02);
 
 
             base.OnRender(drawingContext);
